e16a45265a631ea00648340c58883e095ba07592
GalantAP <galantpratama17@gmail.com>
2024-09-27 18:21:22 +0700

    Third Commit

--- PBJ3/AplikasiCuaca/cekCuaca.js
+++ PBJ3/AplikasiCuaca/cekCuaca.js
@@ -1,5 +1,5 @@
 const request = require('postman-request');
-const urlCuaca ='https://api.weatherstack.com/current?access_key=1e9da06300987ae65eb2b8a29c844ad9&query=-0.888046579860923,%20100.35332123901476&units=m';
+const urlCuaca ='https://api.weatherstack.com/current?access_key=1e9da06300987ae65eb2b8a29c844ad9&query=-0.888046579860923,%20100.35332123901476&units=f';
 
 request({ url: urlCuaca, json: true }, (error, response) => {
     if (error) {
